c9edda5a942fe02a34fc3ea5596cf7a9297f0f49
aballenghien <audrey.ballenghien@gmail.com>
2017-04-04 10:01:02 +0200

    modif des packages

--- src/plateforme/Loader.java
+++ src/plateforme/Loader.java
@@ -8,30 +8,30 @@ import java.util.ArrayList;
 import java.util.Properties;
 
 public class Loader {
-	public static Object getPlugin(String filename, ArrayList<Class<?>> contrainte) throws FileNotFoundException,
-			IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-		// charger le fichier
-		Properties p = new Properties();
-		InputStream is = new FileInputStream(filename);
-		p.load(is);
-		ArrayList<Object> listeobjet = new ArrayList<Object>();
-		// cr�er le beans
-		String valeur = p.getProperty("class");
-		String[] test = valeur.split(";");
-		for (String s : test) {
-
-			Class<?> cl = Class.forName(s);
-			for (Class<?> classe : contrainte) {
-
-				if (classe.isAssignableFrom(cl)) {
-					Object o = cl.newInstance();
-					listeobjet.add(o);
-
-				}
-
-			}
-		}
-
-		return listeobjet;
-	}
+//	public static Object getPlugin(String filename, ArrayList<Class<?>> contrainte) throws FileNotFoundException,
+//			IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+//		// charger le fichier
+//		Properties p = new Properties();
+//		InputStream is = new FileInputStream(filename);
+//		p.load(is);
+//		ArrayList<Object> listeobjet = new ArrayList<Object>();
+//		// cr�er le beans
+//		String valeur = p.getProperty("class");
+//		String[] test = valeur.split(";");
+//		for (String s : test) {
+//
+//			Class<?> cl = Class.forName(s);
+//			for (Class<?> classe : contrainte) {
+//
+//				if (classe.isAssignableFrom(cl)) {
+//					Object o = cl.newInstance();
+//					listeobjet.add(o);
+//
+//				}
+//
+//			}
+//		}
+//
+//		return listeobjet;
+//	}
 }
