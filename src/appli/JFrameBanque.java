499c375712fae98f64a07befaaf04bba712f57fb
abouslam <bouslama.alaeeddine@gmail.com>
2017-04-26 17:02:46 +0200

    Afficher
    Change-Id: I2b7202672d1211f4ea24bfeafe34c9ff4aa9e118

--- src/appli/JFrameBanque.java
+++ src/appli/JFrameBanque.java
@@ -12,13 +12,15 @@ import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
 
 import appli.data.Banque;
 import appli.data.Client;
 import appli.data.Compte;
 import appli.data.IAfficheur;
 import appli.data.IDescription;
-import javax.swing.table.DefaultTableModel;
 import plateforme.Loader;
 
 /**
@@ -38,25 +40,29 @@ public class JFrameBanque extends javax.swing.JFrame {
 
 	public void afficherCompte(Client client) {
 		IAfficheur afficheur = (IAfficheur) Loader.getInstance().getPlugin(this.getListDescriptionPluging().get(1));
-		ArrayList<String> donnes = null;
-		ArrayList<String> entetes = afficheur.afficherEntete();
+		ArrayList<String> entete = afficheur.afficherEntete();
+		DefaultTableModel tableModel = (DefaultTableModel) this.jTableCompte.getModel();
+		tableModel.setColumnIdentifiers(entete.toArray());
+		ArrayList<String> compte = new ArrayList();
+
 		for (Compte list : client.getComptes()) {
-			donnes.addAll(afficheur.afficher((Compte) list));
+			compte = afficheur.afficher(list);
+			tableModel.addRow(compte.toArray());
 		}
-		this.setjTableCompte(entetes, donnes);
+		jTableCompte.setModel(tableModel);
 	}
 
 	public void afficherClient(Banque banque) {
 		IAfficheur afficheur = (IAfficheur) Loader.getInstance().getPlugin(this.getListDescriptionPluging().get(0));
-                ArrayList<String> entete = afficheur.afficherEntete();
-                DefaultTableModel tableModel = (DefaultTableModel) this.jTableClient.getModel();
-                tableModel.setColumnIdentifiers(entete.toArray());
-                ArrayList<String> unClient = new ArrayList();
-                for(Client c : banque.getClients()){
-                    unClient = afficheur.afficher(c);
-                    tableModel.addRow(unClient.toArray());
-                }
-                jTableClient.setModel(tableModel);
+		ArrayList<String> entete = afficheur.afficherEntete();
+		DefaultTableModel tableModel = (DefaultTableModel) this.jTableClient.getModel();
+		tableModel.setColumnIdentifiers(entete.toArray());
+		ArrayList<String> unClient = new ArrayList();
+		for (Client c : banque.getClients()) {
+			unClient = afficheur.afficher(c);
+			tableModel.addRow(unClient.toArray());
+		}
+		jTableClient.setModel(tableModel);
 	}
 
 	/**
@@ -190,6 +196,13 @@ public class JFrameBanque extends javax.swing.JFrame {
 				Loader loader = Loader.getInstance();
 				j.setListDescriptionPluging(loader.getPluginsDescriptions(IAfficheur.class));
 				j.afficherClient(banque);
+				j.getjTableClient().getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+
+					@Override
+					public void valueChanged(ListSelectionEvent e) {
+						j.afficherCompte(banque.chercherClient(j.getjTableClient().getSelectedRow()));
+					}
+				});
 				j.setVisible(true);
 			}
 		});
