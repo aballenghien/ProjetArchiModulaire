8097265c60aab1e081dc6d7b7a0b81fd5dc67470
abouslam <bouslama.alaeeddine@gmail.com>
2017-04-26 17:27:43 +0200

    Change-Id: I29683157099dea3fb6180aec3703c372c82a543b

--- src/plateforme/Loader.java
+++ src/plateforme/Loader.java
@@ -1,87 +1,87 @@
 package plateforme;
 
-import appli.data.IDescription;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Optional;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import appli.data.IDescription;
 import plugins.DescriptionPlugins;
 
 public class Loader {
 
-    private static Loader loader = null;
-    private final String NOM_REPERTOIRE = "D:\\DOCUMENTS\\DEVELOPPEMENT\\NET BEANS\\Projet_Architecture_modulaire\\src\\appli\\config";
-    private final ArrayList<IDescription> lstDescPlugins;
+	private static Loader loader = null;
+	private final String NOM_REPERTOIRE = "D:\\DOCUMENTS\\DEVELOPPEMENT\\NET BEANS\\Projet_Architecture_modulaire\\src\\appli\\config";
+	private final ArrayList<IDescription> lstDescPlugins;
 
-    private Loader() {
-        this.lstDescPlugins = new ArrayList<>();
-        loadPluginsDescriptions();
-    }
+	private Loader() {
+		this.lstDescPlugins = new ArrayList<>();
+		loadPluginsDescriptions();
+	}
 
-    public static Loader getInstance() {
-        if (loader == null) {
-            loader = new Loader();
-        }
-        return loader;
-    }
+	public static Loader getInstance() {
+		if (loader == null) {
+			loader = new Loader();
+		}
+		return loader;
+	}
 
-    private void loadPluginsDescriptions() {
-        File file = new File(NOM_REPERTOIRE);
-        File[] files = file.listFiles();
-        if (files != null) {
-            for (File f : files) {
-                Properties p = new Properties();
-                try {
-                    InputStream is = new FileInputStream(f);
-                    p.load(is);
-                    String nom = p.getProperty("nom");
-                    String contrainte = p.getProperty("contrainte");
-                    String classe = p.getProperty("class");
-                    DescriptionPlugins desc = new DescriptionPlugins(nom, classe, contrainte);
-                    this.lstDescPlugins.add(desc);
-                    
-                } catch (FileNotFoundException ex) {
-                    Logger.getLogger(Loader.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IOException ex) {
-                    Logger.getLogger(Loader.class.getName()).log(Level.SEVERE, null, ex);
-                }
+	private void loadPluginsDescriptions() {
+		File file = new File(NOM_REPERTOIRE);
+		File[] files = file.listFiles();
+		if (files != null) {
+			for (File f : files) {
+				Properties p = new Properties();
+				try {
+					InputStream is = new FileInputStream(f);
+					p.load(is);
+					String nom = p.getProperty("nom");
+					String contrainte = p.getProperty("contrainte");
+					String classe = p.getProperty("class");
+					DescriptionPlugins desc = new DescriptionPlugins(nom, classe, contrainte);
+					this.lstDescPlugins.add(desc);
 
-            }
-        }
+				} catch (FileNotFoundException ex) {
+					Logger.getLogger(Loader.class.getName()).log(Level.SEVERE, null, ex);
+				} catch (IOException ex) {
+					Logger.getLogger(Loader.class.getName()).log(Level.SEVERE, null, ex);
+				}
 
-    }
+			}
+		}
 
-    public ArrayList<IDescription> getPluginsDescriptions(Class<?> contrainte) {
-        ArrayList<IDescription> lstPluginDescriptions = new ArrayList<>();
-        for (IDescription iDesc : this.lstDescPlugins) {
-            if (contrainte.getName().equals(iDesc.getDescContrainte())) {
-                lstPluginDescriptions.add(iDesc);
-            }
+	}
 
-        }
-        return lstPluginDescriptions;
-    }
+	public ArrayList<IDescription> getPluginsDescriptions(Class<?> contrainte) {
+		ArrayList<IDescription> lstPluginDescriptions = new ArrayList<>();
+		for (IDescription iDesc : this.lstDescPlugins) {
+			if (contrainte.getName().equals(iDesc.getDescContrainte())) {
+				lstPluginDescriptions.add(iDesc);
+			}
 
-    public Object getPlugin(IDescription desc) {
-        Object o = null;
-        try {
+		}
+		return lstPluginDescriptions;
+	}
 
-            Class<?> cl = Class.forName(desc.getDescClasse());
-            Class<?> contrainte = Class.forName(desc.getDescContrainte());
-            if (contrainte.isAssignableFrom(cl)) {
-                o = cl.newInstance();
-            }
+	public Object getPlugin(IDescription desc) {
+		Object o = null;
+		try {
 
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-            Logger.getLogger(Loader.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return o;
+			Class<?> cl = Class.forName(desc.getDescClasse());
+			Class<?> contrainte = Class.forName(desc.getDescContrainte());
+			if (contrainte.isAssignableFrom(cl)) {
+				o = cl.newInstance();
+			}
 
-    }
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+			Logger.getLogger(Loader.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return o;
+
+	}
 }
